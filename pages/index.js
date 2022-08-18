8017ce7d40fb7b9be6594b37996eeab868a9e827
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-18 17:06:11 -0400

    changed todoList to be a const

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 export default function Home() {
-  const [todoList, setTodoList] = useState([
+  const todoList = [
     { id: "59bieMO6JsBbN-5", item: "water the plants" },
     { id: "oYLHf2rIAfm7MAS", item: "do the laundry" },
     {
       id: "QqzYOVmlhuLqf_y",
       item: "read 50 pages of a book (dhivas recommends the steve jobs biography)",
     },
-  ]);
+  ];
 }
