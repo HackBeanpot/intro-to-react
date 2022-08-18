a6336bfc1d12b9865ce566af51a373d2c765478d
jzhzhang66 <jzhzhang66@gmail.com>
2022-08-18 19:23:10 -0400

    added excalmations after first todo item

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 export default function Home() {
   const todoList = [
-    { id: "59bieMO6JsBbN-5", item: "water the plants" },
+    { id: "59bieMO6JsBbN-5", item: "water the plants!!!!!!" },
     { id: "oYLHf2rIAfm7MAS", item: "do the laundry" },
     {
       id: "QqzYOVmlhuLqf_y",
